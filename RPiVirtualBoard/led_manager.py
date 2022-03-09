93397beff1fc662b57d4836013abafc3153db208
LeonLimnes <francotzar@yahoo.com>
2022-03-08 18:56:04 -0600

    Cambio led manager
    Se agregaron for loops al código de led manager

--- RPiVirtualBoard/led_manager.py
+++ RPiVirtualBoard/led_manager.py
@@ -19,100 +19,56 @@ import RPi.GPIO as GPIO
 # Imports sleep functon
 from time import sleep
 # Initializes virtual board (comment out for hardware deploy)
-import virtualboard
+#import virtualboard
 
 
+#Array con los pines de los leds
+leds_array = [12,16,18,22,24,26,32]
+
+disp_array = [36,37,38,40]
+
+# Set up Rpi.GPIO library to use physical pin numbers
 GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(26, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(24, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(36, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(38, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(40, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(37, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(22, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(32, GPIO.OUT, initial=GPIO.LOW)
+for led in leds_array:
+    GPIO.setup(led, GPIO.OUT, initial=GPIO.LOW)
+for disp in disp_array:
+    GPIO.setup(disp, GPIO.OUT, initial=GPIO.LOW)
 
 
 #Casos para encender los LEDS
 def leds(num):
+        for led in leds_array:
+            GPIO.output(led, GPIO.LOW)  # Turn led off
         if num == 7:
-                GPIO.output(32, GPIO.HIGH) # Turn led           #Led 7
-                GPIO.output(24, GPIO.LOW)  # Turn led off
-                GPIO.output(22, GPIO.LOW)  # Turn led off
-                GPIO.output(18, GPIO.LOW)  # Turn led off
-                GPIO.output(16, GPIO.LOW)  # Turn led off
-                GPIO.output(12, GPIO.LOW)  # Turn led off
-                GPIO.output(10, GPIO.LOW)  # Turn led off
-                
+            GPIO.output(32, GPIO.HIGH)  # Turn led
+
         if num == 6:
-                GPIO.output(26, GPIO.HIGH) # Turn led        #Led 6
-                GPIO.output(26, GPIO.LOW)  # Turn led off
-                GPIO.output(22, GPIO.LOW)  # Turn led off
-                GPIO.output(18, GPIO.LOW)  # Turn led off
-                GPIO.output(16, GPIO.LOW)  # Turn led off
-                GPIO.output(12, GPIO.LOW)  # Turn led off
-                GPIO.output(10, GPIO.LOW)  # Turn led off
+            GPIO.output(26, GPIO.HIGH)  # Turn led
 
         if num == 5:
-                GPIO.output(24, GPIO.HIGH) # Turn led        #Led 5
-                GPIO.output(26, GPIO.LOW)  # Turn led off
-                GPIO.output(24, GPIO.LOW)  # Turn led off
-                GPIO.output(18, GPIO.LOW)  # Turn led off
-                GPIO.output(16, GPIO.LOW)  # Turn led off
-                GPIO.output(12, GPIO.LOW)  # Turn led off
-                GPIO.output(10, GPIO.LOW)  # Turn led off
+            GPIO.output(24, GPIO.HIGH)  # Turn led
 
         if num == 4:
-                GPIO.output(22, GPIO.HIGH) # Turn led         #Led 4
-                GPIO.output(26, GPIO.LOW)  # Turn led off
-                GPIO.output(24, GPIO.LOW)  # Turn led off
-                GPIO.output(22, GPIO.LOW)  # Turn led off
-                GPIO.output(16, GPIO.LOW)  # Turn led off
-                GPIO.output(12, GPIO.LOW)  # Turn led off
-                GPIO.output(10, GPIO.LOW)  # Turn led off
+            GPIO.output(22, GPIO.HIGH)  # Turn led
 
         if num == 3:
-                GPIO.output(18, GPIO.HIGH) # Turn led     #Led 3
-                GPIO.output(26, GPIO.LOW)  # Turn led off
-                GPIO.output(24, GPIO.LOW)  # Turn led off
-                GPIO.output(22, GPIO.LOW)  # Turn led off
-                GPIO.output(18, GPIO.LOW)  # Turn led off
-                GPIO.output(12, GPIO.LOW)  # Turn led off
-                GPIO.output(10, GPIO.LOW)  # Turn led off
+            GPIO.output(18, GPIO.HIGH)  # Turn led
 
         if num == 2:
-                GPIO.output(16, GPIO.HIGH) # Turn led       #Led 2
-                GPIO.output(26, GPIO.LOW)  # Turn led off
-                GPIO.output(24, GPIO.LOW)  # Turn led off
-                GPIO.output(22, GPIO.LOW)  # Turn led off
-                GPIO.output(18, GPIO.LOW)  # Turn led off
-                GPIO.output(16, GPIO.LOW)  # Turn led off
-                GPIO.output(10, GPIO.LOW)  # Turn led off
+            GPIO.output(16, GPIO.HIGH)  # Turn led
 
         if num == 1:
-                GPIO.output(12, GPIO.HIGH) # Turn led    #Led 1
-                GPIO.output(26, GPIO.LOW)  # Turn led off
-                GPIO.output(24, GPIO.LOW)  # Turn led off
-                GPIO.output(22, GPIO.LOW)  # Turn led off
-                GPIO.output(18, GPIO.LOW)  # Turn led off
-                GPIO.output(16, GPIO.LOW)  # Turn led off
-                GPIO.output(10, GPIO.LOW)  # Turn led off
-        pass
+            GPIO.output(12, GPIO.HIGH)  # Turn led
 
 
 
 def bcd(num):
-		GPIO.output(36,GPIO.HIGH if (num & 0x00000001)  > 0 else GPIO.LOW)   
-		GPIO.output(38, GPIO.HIGH if (num & 0x00000002) > 0 else GPIO.LOW)  
+		GPIO.output(36,GPIO.HIGH if (num & 0x00000001)  > 0 else GPIO.LOW)
+		GPIO.output(38, GPIO.HIGH if (num & 0x00000002) > 0 else GPIO.LOW)
 		GPIO.output(40, GPIO.HIGH if (num & 0x00000004) > 0 else GPIO.LOW)
 		GPIO.output(37, GPIO.HIGH if (num & 0x00000008) > 0 else GPIO.LOW)	
-        
-        
+
+
 
 def marquee(type='pingpong'):
 	switcher = {
@@ -126,82 +82,20 @@ def marquee(type='pingpong'):
 
 
 def _marquee_right():
-        GPIO.output(10, GPIO.HIGH)      # Turn led on
-        GPIO.output(12, GPIO.HIGH)      # Turn led on  
+    for led in leds_array:
+        GPIO.output(led, GPIO.LOW)  # Turn led off
+    for led in leds_array:
+        GPIO.output(led, GPIO.HIGH)  # Turn led off
         sleep(0.1)
-        GPIO.output(10, GPIO.LOW)       # Turn led off
+        GPIO.output(led, GPIO.LOW)  # Turn led
+def _marquee_left():
+    for led in leds_array:
+        GPIO.output(led, GPIO.LOW)  # Turn led off
+    for led in leds_array[::-1]:
+        GPIO.output(led, GPIO.HIGH)  # Turn led off
         sleep(0.1)
-        GPIO.output(12, GPIO.HIGH)      # Turn led on
-        GPIO.output(16, GPIO.HIGH)      # Turn led on
-        sleep(0.1)
-        GPIO.output(12, GPIO.LOW)       # Turn led off
-        sleep(0.1)
-        GPIO.output(16, GPIO.HIGH)      # Turn led on
-        GPIO.output(18, GPIO.HIGH)      # Turn led on
-        sleep(0.1)
-        GPIO.output(16, GPIO.LOW)       # Turn led off
-        sleep(0.1)
-        GPIO.output(18, GPIO.HIGH)      # Turn led on
-        GPIO.output(22, GPIO.HIGH)      # Turn led on
-        sleep(0.1)
-        GPIO.output(18, GPIO.LOW)       # Turn led off
-        sleep(0.1)
-        GPIO.output(22, GPIO.HIGH)      # Turn led on
-        GPIO.output(24, GPIO.HIGH)      # Turn led on
-        sleep(0.1)
-        GPIO.output(22, GPIO.LOW)       # Turn led off
-        sleep(0.1)
-        GPIO.output(24, GPIO.HIGH)      # Turn led on
-        GPIO.output(26, GPIO.HIGH)      # Turn led on
-        sleep(0.1)
-        GPIO.output(24, GPIO.LOW)       # Turn led off
-        sleep(0.1)
-        GPIO.output(26, GPIO.HIGH)      # Turn led on
-        GPIO.output(32, GPIO.HIGH)      # Turn led on
-        sleep(0.1)
-        GPIO.output(26, GPIO.LOW)       # Turn led off
-        sleep(0.3)
-        GPIO.output(32, GPIO.LOW)       # Turn led off
-        
-def _marquee_left():                                                  
-        GPIO.output(32, GPIO.HIGH)      # Turn led on		       
-        GPIO.output(26, GPIO.HIGH)      # Turn led on
-        sleep(0.1)
-        GPIO.output(32, GPIO.LOW)       # Turn led off
-        sleep(0.1)
-        GPIO.output(26, GPIO.HIGH)      # Turn led on
-        GPIO.output(24, GPIO.HIGH)      # Turn led on
-        sleep(0.1)
-        GPIO.output(26, GPIO.LOW)       # Turn led off
-        sleep(0.1)
-        GPIO.output(24, GPIO.HIGH)      # Turn led on
-        GPIO.output(22, GPIO.HIGH)      # Turn led on
-        sleep(0.1)
-        GPIO.output(24, GPIO.LOW)       # Turn led off
-        sleep(0.1)
-        GPIO.output(22, GPIO.HIGH)      # Turn led on
-        GPIO.output(18, GPIO.HIGH)      # Turn led on
-        sleep(0.1)
-        GPIO.output(22, GPIO.LOW)       # Turn led off
-        sleep(0.1)
-        GPIO.output(18, GPIO.HIGH)      # Turn led on
-        GPIO.output(16, GPIO.HIGH)      # Turn led on
-        sleep(0.1)
-        GPIO.output(18, GPIO.LOW)       # Turn led off
-        sleep(0.1)
-        GPIO.output(16, GPIO.HIGH)      # Turn led on
-        GPIO.output(12, GPIO.HIGH)      # Turn led on
-        sleep(0.1)
-        GPIO.output(16, GPIO.LOW)       # Turn led off
-        sleep(0.1)
-        GPIO.output(12, GPIO.HIGH)      # Turn led on
-        GPIO.output(10, GPIO.HIGH)      # Turn led on
-        sleep(0.1)
-        GPIO.output(12, GPIO.LOW)       # Turn led off
-        sleep(0.2)
-        GPIO.output(10, GPIO.LOW)       # Turn led off
+        GPIO.output(led, GPIO.LOW)  # Turn led
 
-
-def _marquee_pingpong():  
-	_marquee_right()   
-	_marquee_left()    
+def _marquee_pingpong():
+	_marquee_right()
+	_marquee_left()
